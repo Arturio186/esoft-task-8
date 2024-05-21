2dbe557374f9b53a9b801d033e7a02ec163c2f0f
Arturio186 <bain.romanoff@mail.ru>
2024-05-21 23:30:16 +0500

    UserGenerator

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from "express";
+import UserGenerator from "../Utils/UserGenerator";
+
+interface IUser {
+    id: number,
+    name: string,
+    email: string,
+    age: number
+}
 
 class UserController {
+    private static Users: IUser[] = UserGenerator.generateUsers(100);
+
     static async GetAll(req: Request, res: Response) {
         try {
-            res.json({status: 200, message: 'Get all'});
+            res.json({status: 200, message: UserController.Users});
         }
         catch (error) {
             console.log(error)
